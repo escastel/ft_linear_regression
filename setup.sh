2759a469eb8a67e53dcbeae84a86664f7c6285aa
Ester Castellano Rios <escastel@c3r8s3.42malaga.com>
2025-06-20 14:18:40 +0200

    virtual enviroment setup

---
+++ setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+function install(){
+	echo "🔧 Creating virtual environment ..."
+	python3 -m venv venv
+	if [ $? -ne 0 ]; then
+		echo "❌ Error: Make sure you have Python 3 installed and in your PATH."
+		exit 1
+	fi
+	echo "✅ Virtual environment created."
+
+	echo ""
+	echo "📦 Installing dependencies ..."
+
+	source venv/bin/activate
+
+	if [ ! -f requirements.txt ]; then
+    	echo "pandas" > requirements.txt
+    	echo "matplotlib" >> requirements.txt
+    	echo "📄 requirements.txt created with basic dependencies."
+  	fi
+
+	pip install --upgrade pip
+	pip install -r requirements.txt
+
+	echo ""
+	echo "✅ Dependencies installed."
+	echo "🐍 You are inside the Python virtual environment."
+}
+
+function clean() {
+	echo "🧹 Cleaning up virtual environment and related files ..."
+	rm -rf ./venv __pycache__/
+	echo "✅ Cleaning completed."
+}
+
+function help(){
+	echo "Use: ./setup.sh [comando]"
+	echo ""
+	echo "Available commands:"
+	echo "  install   → Create virtual environment and install dependencies."
+	echo "  clean     → Delete virtual environment and generated files."
+}
+
+case "$1" in
+	install)
+		install
+	;;
+	clean)
+		clean
+	;;
+	*)
+		help
+	;;
+esac
